4de686bcf7bdfd1c638713c20e1aca06922f9c9e
Baker <tonybaker23@ou.edu>
2019-02-11 11:42:44 -0600

    Add project files.

---
+++ HWK2_Baker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Tony Baker
+namespace HWK2_Baker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
---
+++ HWK2_Baker/RecieptClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWK2_Baker
+{
+    class RecieptClass
+    {
+        public int CustomerID { get; set; }
+        public int COGQuantity { get; set; }
+        public int GearQuantity { get; set; }
+        private DateTime SaleDate;
+        private double SaleTaxPercent;
+        private double CogPrice;
+        private double GearPrice;
+
+        public RecieptClass() //Giving Starting values for instances
+        {
+            CustomerID = 0;
+            COGQuantity = 0;
+            GearQuantity = 0;
+        }
+        public
+    }
+}
